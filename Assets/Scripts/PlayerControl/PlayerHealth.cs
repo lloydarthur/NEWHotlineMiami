ffcab9a85da14e7fef34fdbebc5f2a9ce5bca0b3
Mohamud <detectivemomo1@gmail.com>
2016-03-19 15:32:17 -0400

    no real changes

---
+++ Assets/Scripts/PlayerControl/PlayerHealth.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	// Use this for initialization
+	public int startingHeath;
+	public int currentHealth;
+	public Slider HealthSlider;
+	public Image damageImage;
+	public float flashSpeed =5f;
+	public Color flashColour = new Color(1f,0f,0f,0.1f);
+
+	bool damaged;
+
+	void Awake()
+	{
+		currentHealth = startingHeath;
+
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
