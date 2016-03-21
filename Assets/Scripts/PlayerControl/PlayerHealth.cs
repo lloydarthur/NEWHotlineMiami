da1c421d16dd338915002d6970d6f115b11bf540
Mohamud <detectivemomo1@gmail.com>
2016-03-21 09:25:02 -0400

    NO real changes

--- Assets/Scripts/PlayerControl/PlayerHealth.cs
+++ Assets/Scripts/PlayerControl/PlayerHealth.cs
@@ -5,26 +5,74 @@ using System.Collections;
 public class PlayerHealth : MonoBehaviour {
 
 	// Use this for initialization
-	public int startingHeath;
+	public int startingHealth;
 	public int currentHealth;
 	public Slider HealthSlider;
 	public Image damageImage;
 	public float flashSpeed =5f;
 	public Color flashColour = new Color(1f,0f,0f,0.1f);
 
-	bool damaged;
+//	Animator anim;                                              // Reference to the Animator component.
+//	AudioSource playerAudio;                                    // Reference to the AudioSource component.
+//	PlayerMovement playerMovement;                              // Reference to the player's movement.
+//	PlayerShooting playerShooting;                              // Reference to the PlayerShooting script.
+ //   bool isDead;                                                // Whether the player is dead.
+	bool damaged;                                               // True when the player gets damaged.
 
-	void Awake()
+
+	void Awake ()
 	{
-		currentHealth = startingHeath;
+		// Setting up the references.
+		//anim = GetComponent <Animator> ();
+	///	playerAudio = GetComponent <AudioSource> ();
+	//	playerMovement = GetComponent <PlayerMovement> ();
+	//	playerShooting = GetComponentInChildren <PlayerShooting> ();
 
+		// Set the initial health of the player.
+		currentHealth = startingHealth;
 	}
-	void Start () {
-	
+
+
+	void Update ()
+	{
+		// If the player has just been damaged...
+		if(damaged)
+		{
+			// ... set the colour of the damageImage to the flash colour.
+			damageImage.color = flashColour;
+		}
+		// Otherwise...
+		else
+		{
+			// ... transition the colour back to clear.
+			damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+		}
+
+		// Reset the damaged flag.
+		damaged = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+
+	public void TakeDamage (int amount)
+	{
+		// Set the damaged flag so the screen will flash.
+		damaged = true;
+
+		// Reduce the current health by the damage amount.
+		currentHealth -= amount;
+
+		// Set the health bar's value to the current health.
+		HealthSlider.value = currentHealth;
+
+		// Play the hurt sound effect.
+		//playerAudio.Play ();
+
+		// If the player has lost all it's health and the death flag hasn't been set yet...
+		//if(currentHealth <= 0 && !isDead)
+		//{
+			// ... it should die.
+		//	Death ();
+///		}
 	}
+
 }
