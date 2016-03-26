95c4d670cd7e6d024fb7548aa25db7160d0c47ef
Mohamud <detectivemomo1@gmail.com>
2016-03-26 16:44:54 -0400

    add shop gui

---
+++ Assets/Scripts/shop/shop.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class shop : MonoBehaviour {
+	// enter the shop
+	public Image EnterShop = null;
+	public Button YesButton = null;
+	public Button NoButton = null;
+	public Text EnterShop_text = null;
+	// shop
+	public Image Shop = null;
+	public ScrollRect ScrollView= null;
+	public Button BuyButton = null;
+	public Button ExistButton = null;
+	public Image Description = null;
+	public GameObject[] items = null;
+	public Image Descriptionborder = null;
+	public Image ScrollbarA = null;
+	public Image handle= null;
+	void intializeShopComponents() 
+	{
+		if (EnterShop == null) {
+			EnterShop = GameObject.FindGameObjectWithTag ("EnterShop").GetComponent<Image>();
+		} else {
+
+		}
+		if (YesButton == null) {
+			YesButton = GameObject.FindGameObjectWithTag ("YesButton").GetComponent<Button>();
+		} else {
+
+		}
+		if (NoButton == null) {
+			NoButton = GameObject.FindGameObjectWithTag ("NoButton").GetComponent<Button>();
+		} else {
+
+		}
+		if (EnterShop_text== null) {
+			EnterShop_text= GameObject.FindGameObjectWithTag ("EnterShopText").GetComponent<Text>();
+		} else {
+
+		}
+			
+	}
+	// Use this for initialization
+	void Start () 
+	{
+		//ExistTheShop ();
+		closeShop ();
+	}
+
+
+		void EnterTheShop()
+	{
+		EnterShop.enabled = true;
+		EnterShop_text.enabled = true;
+		YesButton.GetComponent<Button>().enabled = true;
+		YesButton.GetComponent<Image>().enabled = true;
+		YesButton.GetComponentInChildren<Text>().enabled = true;
+		NoButton.GetComponent<Button>().enabled = true;
+		NoButton.GetComponent<Image>().enabled = true;
+		NoButton.GetComponentInChildren<Text>().enabled = true;
+
+
+	}
+
+	public void ExistTheShop()
+	{
+		EnterShop.enabled = false;
+		EnterShop_text.enabled = false;
+		YesButton.GetComponent<Button>().enabled = false;
+		YesButton.GetComponent<Image>().enabled = false;
+		YesButton.GetComponentInChildren<Text>().enabled = false;
+		NoButton.GetComponent<Button>().enabled = false;
+		NoButton.GetComponent<Image>().enabled = false;
+		NoButton.GetComponentInChildren<Text>().enabled = false;
+	}
+
+	public void OnSell()
+	{
+		Shop.enabled = true;
+		BuyButton.GetComponent<Button>().enabled = true;
+		BuyButton.GetComponent<Image>().enabled = true;
+		BuyButton.GetComponentInChildren<Text>().enabled = true;
+		ExistButton.GetComponent<Button>().enabled = true;
+		ExistButton.GetComponent<Image>().enabled = true;
+		ExistButton.GetComponentInChildren<Text>().enabled = true;
+		Description.GetComponent<Image> ().enabled = true;
+		Descriptionborder.GetComponent<Image>().enabled = true;
+		Description.GetComponentInChildren<Text> ().enabled = true;
+		ScrollView.GetComponent<ScrollRect> ().enabled = true;
+		ScrollView.GetComponentInChildren<Button> ().enabled = true;
+		ScrollView.GetComponentInChildren<Image> ().enabled = true;
+		ScrollbarA.GetComponent<Image> ().enabled = true;
+		handle.GetComponent<Image> ().enabled = true;
+		
+	}
+
+	public void closeShop()
+	{
+		Shop.enabled = false;
+		BuyButton.GetComponent<Button>().enabled = false;
+		BuyButton.GetComponent<Image>().enabled = false;
+		BuyButton.GetComponentInChildren<Text>().enabled = false;
+		ExistButton.GetComponent<Button>().enabled = false;
+		ExistButton.GetComponent<Image>().enabled = false;
+		ExistButton.GetComponentInChildren<Text>().enabled = false;
+		Description.GetComponent<Image> ().enabled = false;
+		Descriptionborder.GetComponent<Image>().enabled = false;
+		Description.GetComponentInChildren<Text> ().enabled = false;
+		ScrollView.GetComponent<ScrollRect> ().enabled = false;
+		ScrollView.GetComponentInChildren<Image> ().enabled = false;
+		ScrollView.GetComponentInChildren<ScrollRect> ().enabled = false;
+		//for (int i = 0; i > items.Length; ++i)
+		//{
+		//	items [i].GetComponent<Image> ().enabled = false;
+		//	items [i].GetComponentInChildren<Text> ().enabled = false;
+		//}
+
+		ScrollbarA.GetComponent<Image> ().enabled = false;
+		handle.GetComponent<Image> ().enabled = false;
+
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
